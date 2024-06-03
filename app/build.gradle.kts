c8a28079b237a931eec38b23b74010b4a377edab
Alexander <82713915+EtoZheSandy@users.noreply.github.com>
2024-06-03 18:22:13 +0300

    Добавил библиотеки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-parcelize")
+//    id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
@@ -36,6 +42,29 @@ android {
 }
 
 dependencies {
+
+    //loading button
+    implementation("br.com.simplepass:loading-button-android:2.2.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+
+    //circular image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    //viewpager2 indicatior
+    implementation("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
+
+    //stepView
+    implementation("com.shuhart.stepview:stepview:1.5.1")
+
+    //Android Ktx
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    implementation(libs.firebase.crashlytics)
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
