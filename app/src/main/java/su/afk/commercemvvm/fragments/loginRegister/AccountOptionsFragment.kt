bcfeddbd6f1c0b4c2317df66910190c8e065c059
Sandy <etozhesandy@gmail.com>
2024-06-04 10:31:50 +0300

    Создал приветственный фрагмент

---
+++ app/src/main/java/su/afk/commercemvvm/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package su.afk.commercemvvm.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import su.afk.commercemvvm.R
+
+class AccountOptionsFragment: Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/su/afk/commercemvvm/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package su.afk.commercemvvm.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import su.afk.commercemvvm.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
